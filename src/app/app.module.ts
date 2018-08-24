51556b5edc2b9ffcb00c437dab88c5c027ab256c
Arman Yaraee <arman@yaraee.net>
2018-08-24 19:06:38 +0100

    Migrating help page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HelpPage } from '../pages/help/help';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    HelpPage,
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    HelpPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Nav } from 'ionic-angular';
+import { HelpPage } from './../help/help';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild(Nav) nav: Nav;
+
+  items: any;
+
   constructor(public navCtrl: NavController) {
+    this.items = [
+      { component: HomePage, title: 'Solve', text: 'dive into the word solver', icon: 'play' },
+      { component: HelpPage, title: 'Help', text: 'learn how to use the app', icon: 'information-circle' }
+    ];
 
   }
 
+  openPage(item) {
+    this.navCtrl.push(item.component);
+  }
 }
